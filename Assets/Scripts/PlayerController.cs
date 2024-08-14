d731f727877f01db2f8cc25a4ad86fa4442db942
Trichor <140474490+Trichor@users.noreply.github.com>
2024-08-14 12:01:49 +0800

    Added movement card function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,13 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float movementSpeed = 1f;
-    [SerializeField] int moveDistance = 3;
+    [SerializeField] int moveDistance = 2;
     [SerializeField] Material movableTile;
     [SerializeField] Material nonMovableTile;
+
+    private Vector2Int currentLocation;
+    private Vector2Int selectedLocation;
+    private bool isMoving = false;
 
     Transform selectedUnit;
     List<Vector2Int> movableCords;
@@ -24,6 +28,8 @@ public class PlayerController : MonoBehaviour
         gridManager = FindObjectOfType<GridManager>();
         movableCords = new List<Vector2Int>();
         selectedUnit = GameObject.FindGameObjectWithTag("Player").transform;
+        currentLocation = new Vector2Int();
+        selectedLocation = new Vector2Int();
 
         PopulateMovableArea();
         EnableMovableTiles();
@@ -33,6 +39,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+
+
+        //runs is movement card is selected
+        /*if (isMoving)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                bool hasHit = Physics.Raycast(ray, out hit);
+
+                if (hasHit)
+                {
+                    if (hit.transform.tag == "Tile")
+                    {
+                        selectedLocation = hit.transform.GetComponent<Labeller>().cords;
+                        selectedUnit.transform.position = new Vector3(selectedLocation.x, selectedUnit.position.y, selectedLocation.y);
+                    }
+                }
+            }
+        }*/
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -60,6 +89,29 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void MovementCard()
+    {
+        PopulateMovableArea();
+        EnableMovableTiles();
+        currentLocation.x = (int)selectedUnit.position.x;
+        currentLocation.y = (int)selectedUnit.position.z;
+        isMoving = true;
+    }
+
+    public void CancelMovementCard()
+    {
+        DisableMovableTiles();
+        selectedUnit.transform.position = new Vector3(currentLocation.x, selectedUnit.position.y, currentLocation.y);
+        isMoving = false;
+    }
+
+    public void ConfirmMovementCard()
+    {
+        DisableMovableTiles();
+
+        isMoving = false;
+    }
+
     private void PopulateMovableArea()
     {
         movableCords.Clear();
@@ -67,9 +119,9 @@ public class PlayerController : MonoBehaviour
         int yCord = (int)selectedUnit.position.z;
 
 
-        for (int x = 0; x < moveDistance; x++)
+        for (int x = 0; x < (moveDistance+1); x++)
         {
-            for (int y = 0; y < moveDistance - x; y++)
+            for (int y = 0; y < (moveDistance+1) - x; y++)
             {
                 int newXCord = xCord + x;
                 int newYCord = yCord + y;
