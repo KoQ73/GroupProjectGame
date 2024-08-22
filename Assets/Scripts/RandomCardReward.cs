ca02a59dbf4f1304ecece90b49b19512ed9a761a
BentleyGoh <bentleygoh@gmail.com>
2024-08-22 14:39:26 +0800

    Update RandomCardReward.cs

--- Assets/Scripts/RandomCardReward.cs
+++ Assets/Scripts/RandomCardReward.cs
@@ -1,28 +1,47 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class RewardUI : MonoBehaviour
+public class RandomCardReward : MonoBehaviour
 {
-    public Image cardReward1;          // Reference to the first card Image component
-    public Image cardReward2;          // Reference to the second card Image component
-    public Sprite[] cardSprites;       // Array of sprites to choose from
+    public Button cardReward1;          // Reference to the first card Button component
+    public Button cardReward2;          // Reference to the second card Button component
+
+    private CardManager cardManager;   // Reference to the CardManager
 
     void Start()
     {
-        AssignRandomCards();
-    }
+        // Find the CardManager in the scene
+        cardManager = FindObjectOfType<CardManager>();
 
-    void AssignRandomCards()
-    {
-        if (cardSprites.Length > 0)
+        if (cardManager != null)
         {
-            // Assign a random sprite to cardReward1
-            int randomIndex1 = Random.Range(0, cardSprites.Length);
-            cardReward1.sprite = cardSprites[randomIndex1];
-
-            // Assign a different random sprite to cardReward2
-            int randomIndex2 = Random.Range(0, cardSprites.Length);
-            cardReward2.sprite = cardSprites[randomIndex2];
+            AssignRandomCards();
+        }
+        else
+        {
+            Debug.LogError("CardManager not found!");
         }
     }
+
+    public void AssignRandomCards()
+    {
+        if (cardManager.availableCards.Count > 0)
+        {
+            // Assign a random card from availableCards to cardReward1
+            int randomIndex1 = Random.Range(0, cardManager.availableCards.Count);
+            Sprite sprite1 = cardManager.availableCards[randomIndex1].cardSprite;
+            cardReward1.GetComponent<Image>().sprite = sprite1;  // Assign sprite to the Image component of Button
+
+            // Assign a different random card from availableCards to cardReward2
+            int randomIndex2 = Random.Range(0, cardManager.availableCards.Count);
+            Sprite sprite2 = cardManager.availableCards[randomIndex2].cardSprite;
+            cardReward2.GetComponent<Image>().sprite = sprite2;  // Assign sprite to the Image component of Button
+        }
+        else
+        {
+            Debug.LogError("No cards available in availableCards list!");
+        }
+    }
+
 }
